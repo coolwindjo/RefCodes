e9950a7ab8fde64b2adf2c9c53dc5ef440c78dac
coolwindjo <coolwind@hotmail.co.kr>
2019-12-24 11:28:40 +0900

    modify ProbSolvStart

--- AlgoGuruProject/ToDo/ProbSolvStart/ProbSolvStart.cpp
+++ AlgoGuruProject/ToDo/ProbSolvStart/ProbSolvStart.cpp
@@ -19,10 +19,10 @@ typedef enum {
 constexpr int DIR[eDIR_LEN][2] = { {0,1}, {1,0}, {0,-1}, {-1,0} };
 
 
-constexpr int NUM_HOUSES = 8;
+// constexpr int NUM_HOUSES = 8;
 class CProbSolv
 {
-    vvi m_vviDualStates;
+    // vvi m_vviDualStates;
 public:
     CProbSolv()
     {
@@ -31,71 +31,35 @@ public:
     ~CProbSolv(){}
 private:
     void _Solve(){
-        int days = 0;
-        int states[NUM_HOUSES];
 
-        vi viStates;
+        // vi viStates;
         // FOR(s, NUM_HOUSES){
         //     int state;
         //     cin >> state;
         //     viStates.push_back(state);
         // }
-        m_vviDualStates.push_back(viStates);
         // m_vviDualStates.push_back(viStates);
-        // cin >> days;
+        // m_vviDualStates.push_back(viStates);
 
+        // int days = 0;
+        // cin >> days;
+        // int states[NUM_HOUSES];
         // FOR(i, NUM_HOUSES){
         //     states[i] = viStates[i];
         // }
-
-        vi viAns = cellCompete(states, days);
+        // vi viAns = cellCompete(states, days);
+        // for(auto ans : viAns){
+        //     cout << ans << " ";
+        // }
         
-        for(auto ans : viAns){
-            cout << ans << " ";
-        }
     }
 
-    vector<int> cellCompete(int* states, int days){
-        
-        // FOR(i, NUM_HOUSES){
-        //     m_vviDualStates[0][i] = states[i];
-        // }
+    // vector<int> cellCompete(int* states, int days){
+    //     const int resIdx = days%2;
+    //     return m_vviDualStates[resIdx];
+    // }
 
-        // FOR(d, days){
-        //     const int i = d%2;
-        //     const int ni = (d+1)%2;
-        //     FOR(h, NUM_HOUSES){
-        //         if(h == 0){
-        //             if(m_vviDualStates[i][h+1] == 0){
-        //                 m_vviDualStates[ni][h] = 0;
-        //             }
-        //             else{
-        //                 m_vviDualStates[ni][h] = 1;
-        //             }
-        //         }
-        //         else if(h == (NUM_HOUSES-1)){
-        //             if(m_vviDualStates[i][h-1] == 0){
-        //                 m_vviDualStates[ni][h] = 0;
-        //             }
-        //             else{
-        //                 m_vviDualStates[ni][h] = 1;
-        //             }
-        //         }
-        //         else{
-        //             if(m_vviDualStates[i][h-1] == m_vviDualStates[i][h+1]){
-        //                 m_vviDualStates[ni][h] = 0;
-        //             }
-        //             else{
-        //                 m_vviDualStates[ni][h] = 1;
-        //             }
-        //         }
-        //     }
-        // }
-        const int resIdx = days%2;
-        return m_vviDualStates[resIdx];
-    }
 };
-
 
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(nullptr);
