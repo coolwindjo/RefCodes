6126bb6eff0d233dfd6b1e43a9c8b4d1ec8dbc45
coolwindjo <coolwind@hotmail.co.kr>
2020-08-04 08:49:25 +0900

    add dockerfiles

--- CodeReferences/Dockerfiles/android-container/build.sh
+++
@@ -1,6 +0,0 @@
-docker build \
---build-arg GRADLE_VERSION=6.5.1 \
---build-arg ANDROID_API_LEVEL=30 \
---build-arg ANDROID_BUILD_TOOLS_LEVEL=30.0.4 \
---build-arg EMULATOR_NAME=test \
--t android-container:sunflower . 
