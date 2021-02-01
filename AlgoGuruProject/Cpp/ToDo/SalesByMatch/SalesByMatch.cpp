1d4d24749ec95b3d8c0ab80e2bce023350b3120f
조승현/선임연구원/AVS Front Camera Unit(seunghyeon.jo@lge.com) <seunghyeon.jo@lge.com>
2021-02-01 20:27:59 +0900

    update algoguru projtec

--- AlgoGuruProject/Cpp/ToDo/SalesByMatch/SalesByMatch.cpp
+++ AlgoGuruProject/Cpp/ToDo/SalesByMatch/SalesByMatch.cpp
@@ -19,70 +19,15 @@ static const int WarnIfNot(const int condFlag, const char* condition){
 }
 #define W_IFNOT(cond) WarnIfNot((cond), (#cond))
 #define P_IFNOT(cond, var) if (!W_IFNOT(cond)) cout << "= " << var <<endl;
-/*/
+/*/ 
 #include "../../ProbSolvStart.h"
 //*/
-
-#if 0   // 12dd
-typedef enum {
-	eR=0, eD,
-    eL, eU,
-    eDIR_LEN
-} Dir_e;
-constexpr int DIR[eDIR_LEN][2] = {
-    {0,1}, {1,0},
-    {0,-1}, {-1,0}
-};
-#endif // 0
 
 class ProbSolv
 {
 public:
     ProbSolv()
     {
-#if 0   // 43dd
-        int numLines = 0;
-        cin >> numLines;
-
-        vstr lines;
-        FOR(i, numLines){
-            string line;
-            std::getline(std::cin, line);
-            if(line.length() == 0){
-                i--;
-                continue;
-            }
-            lines.push_back(line);
-        }
-#endif
-
-#if 0
-        int rows = 0;
-        int columns = 0;
-        cin >> rows;
-        cin >> columns;
-
-        vvi grid;
-        FOR(i, rows){
-            vi rowG;
-            FOR(j, columns){
-                int val;
-                cin >> val;
-                rowG.push_back(val);
-            }
-            grid.push_back(rowG);
-        }
-#ifdef TEST
-        cout <<endl;
-        FOR(i, rows){
-            FOR(j, columns){
-                cout << grid[i][j] << " ";
-            }
-            cout <<endl;
-        }
-#endif
-#endif
-        
         _Solve();
     }
     ~ProbSolv(){}
